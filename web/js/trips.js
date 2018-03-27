90a27047c5e92c7630964ec0a57ebae8e9ff7988
Taylor Smith <tsmith@tsmithcreative.com>
2018-03-27 21:15:37 +0000

    Post the new trip values to the server, accept the input, and populate the table with the new row

--- web/js/trips.js
+++ web/js/trips.js
@@ -1,38 +1,42 @@
 (function($){
   $(document).ready(function(){
+
+    function populateTripTable(trip) {
+      console.log(trip);
+      var tripTable = document.getElementById("triplist");
+      var row = tripTable.insertRow(-1);
+
+      // To ensure the right order, select props each instead of iterating
+      ['id', 'label', 'machine_name', 'starttime', 'endtime'].forEach(function(prop, index) {
+
+        // Create the cell
+        var cell = row.insertCell(index);
+
+        // If we're dealing with a time, let's make it easy to read
+        if (prop.indexOf('time') > -1) {
+          var date = new Date(trip[prop] * 1000);
+          var value = document.createTextNode(
+            date.toLocaleString()
+          );
+        }
+        else {
+          // Append a text node to the cell
+          var value = document.createTextNode(trip[prop]);
+        }
+
+        cell.appendChild(value);
+      });
+    }
+
     $.getJSON('/api/trips', function(data) {
       if (data.length) {
-
-        var tripTable = document.getElementById("triplist");
-
-        data.forEach(function(trip) {
-          var row = tripTable.insertRow(-1);
-
-          // To ensure the right order, select props each instead of iterating
-          ['id', 'label', 'machine_name', 'starttime', 'endtime'].forEach(function(prop, index) {
-
-            // Create the cell
-            var cell = row.insertCell(index);
-
-            // If we're dealing with a time, let's make it easy to read
-            if (prop.indexOf('time') > -1) {
-              var date = new Date(trip[prop] * 1000);
-              var value = document.createTextNode(
-                date.toLocaleString()
-              );
-            }
-            else {
-              // Append a text node to the cell
-              var value = document.createTextNode(trip[prop]);
-            }
-
-            cell.appendChild(value);
-          });
-        });
+        data.forEach(populateTripTable);
       }
     });
 
-    $('#trip-form').on("submit", function(){
+    $('#trip-form').on("submit", function(e){
+      e.preventDefault();
+
       var newTrip = {
         id: null,
         machine_name: document.getElementById("trip_machine_name").value,
@@ -41,7 +45,16 @@
         label: document.getElementById("trip_label").value
       }
 
-      console.log(newTrip);
+      $.ajax({
+        type: "POST",
+        url: "/api/trips/create",
+        data: JSON.stringify(newTrip),
+        success: function () {
+          $('#trip-form input').val("");
+        }
+      }).done(function (data) {
+        populateTripTable(JSON.parse(data));
+      });
     });
   });
 })(jQuery);
