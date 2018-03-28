6354ee0e224bf09193023c2303752f2f03a305d6
Taylor Smith <tsmith@tsmithcreative.com>
2018-03-28 01:41:38 +0000

    Drop Moment.js requirement for the home page, toLocaleString can do this and it's better

--- web/js/home.js
+++ web/js/home.js
@@ -6,9 +6,8 @@
 
     if (data.city.length) {
       // Convert the timestamp to a localized date string; display:
-      var lastTime = moment.unix(data.time);
-      var lastTimeOutput = lastTime.clone().tz("America/Chicago");
-      $('#last-time').text(lastTimeOutput.format('dddd, MMMM Do, h:mma z'));
+      var lastTime = new Date(data.time * 1000);
+      $('#last-time').text(lastTime.toLocaleString());
 
       // Output the city name
       $('#last-location').text(data.city);
